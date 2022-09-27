b846993ed8b9dcd9560ff205a8aa0d2a62adcb94
Egor Kartashov <egor.linkinked@gmail.com>
2022-09-27 14:17:13 +0500

    TG bot base from 1st workshop

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"gitlab.ozon.dev/egor.linkinked/kartashov-egor/internal/clients/tg"
+	"gitlab.ozon.dev/egor.linkinked/kartashov-egor/internal/config"
+	"gitlab.ozon.dev/egor.linkinked/kartashov-egor/internal/model/messages"
+)
+
+func main() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed: ", err)
+	}
+
+	tgClient, err := tg.New(cfg)
+	if err != nil {
+		log.Fatal("tg client init failed: ", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
